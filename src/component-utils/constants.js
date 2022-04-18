88bd42298c3b1854f46906b2b864dbc431a7fee4
matthew-paul <matthew.paul.mail@gmail.com>
2022-04-17 21:25:08 -0500

    added fracture map

--- src/component-utils/constants.js
+++ src/component-utils/constants.js
@@ -1,71 +1,72 @@
-import ReconBolt from '../resources/Agents/Sova/Recon_Bolt.png'
-import ShockBolt from '../resources/Agents/Sova/Shock_Bolt.png'
-import SnakeBite from '../resources/Agents/Viper/Snake_Bite.png'
-import AscentMap from '../resources/Maps/ascent_map.png'
-import BindMap from '../resources/Maps/bind_map.png'
-import BreezeMap from '../resources/Maps/breeze_map.png'
-import HavenMap from '../resources/Maps/haven_map.png'
-import IceboxMap from '../resources/Maps/icebox_map.png'
-import SplitMap from '../resources/Maps/split_map.png'
+import ReconBolt from "../resources/Agents/Sova/Recon_Bolt.png";
+import ShockBolt from "../resources/Agents/Sova/Shock_Bolt.png";
+import SnakeBite from "../resources/Agents/Viper/Snake_Bite.png";
+import AscentMap from "../resources/Maps/ascent_map.png";
+import BindMap from "../resources/Maps/bind_map.png";
+import BreezeMap from "../resources/Maps/breeze_map.png";
+import HavenMap from "../resources/Maps/haven_map.png";
+import IceboxMap from "../resources/Maps/icebox_map.png";
+import SplitMap from "../resources/Maps/split_map.png";
+import FractureMap from "../resources/Maps/fracture_map.png";
 
-export const API_URL = 'https://cxvofre6jj.execute-api.us-east-1.amazonaws.com/lineups'
+export const API_URL =
+  "https://cxvofre6jj.execute-api.us-east-1.amazonaws.com/lineups";
 
-export const localStorageExpirationTime = 1000 * 60 * 60 * 24;   // How long localStorage will keep lineup info before refreshing in milliseconds
+export const localStorageExpirationTime = 1000 * 60 * 60 * 24; // How long localStorage will keep lineup info before refreshing in milliseconds
 //                                        ms     s    min  hr
 
 export const AGENT_LIST = [
-    { value: 1, label: "Astra" },
-    { value: 2, label: "Breach" },
-    { value: 3, label: "Brimstone" },
-    { value: 4, label: "Cypher" },
-    { value: 5, label: "Jett" },
-    { value: 6, label: "Killjoy" },
-    { value: 16, label: "Kay/O" },
-    { value: 7, label: "Omen" },
-    { value: 8, label: "Phoenix" },
-    { value: 9, label: "Raze" },
-    { value: 10, label: "Reyna" },
-    { value: 11, label: "Sage" },
-    { value: 12, label: "Skye" },
-    { value: 13, label: "Sova" },
-    { value: 14, label: "Viper" },
-    { value: 15, label: "Yoru" },
-]
+  { value: 1, label: "Astra" },
+  { value: 2, label: "Breach" },
+  { value: 3, label: "Brimstone" },
+  { value: 4, label: "Cypher" },
+  { value: 5, label: "Jett" },
+  { value: 6, label: "Killjoy" },
+  { value: 16, label: "Kay/O" },
+  { value: 7, label: "Omen" },
+  { value: 8, label: "Phoenix" },
+  { value: 9, label: "Raze" },
+  { value: 10, label: "Reyna" },
+  { value: 11, label: "Sage" },
+  { value: 12, label: "Skye" },
+  { value: 13, label: "Sova" },
+  { value: 14, label: "Viper" },
+  { value: 15, label: "Yoru" },
+];
 
 export const ABILITY_LIST = {
-    13: [
-        { value: 1, label: "Recon Bolt", icon: ReconBolt },
-        { value: 2, label: "Shock Dart", icon: ShockBolt },
-    ],
-    14: [
-        { value: 1, label: "Snake Bite", icon: SnakeBite }
-    ]
-}
+  13: [
+    { value: 1, label: "Recon Bolt", icon: ReconBolt },
+    { value: 2, label: "Shock Dart", icon: ShockBolt },
+  ],
+  14: [{ value: 1, label: "Snake Bite", icon: SnakeBite }],
+};
 
 export const MAP_LIST = [
-    { value: 1, label: "Ascent", icon: AscentMap },
-    { value: 2, label: "Bind", icon: BindMap },
-    { value: 3, label: "Breeze", icon: BreezeMap },
-    { value: 4, label: "Haven", icon: HavenMap },
-    { value: 5, label: "Icebox", icon: IceboxMap },
-    { value: 6, label: "Split", icon: SplitMap },
-]
+  { value: 1, label: "Ascent", icon: AscentMap },
+  { value: 2, label: "Bind", icon: BindMap },
+  { value: 3, label: "Breeze", icon: BreezeMap },
+  { value: 4, label: "Haven", icon: HavenMap },
+  { value: 5, label: "Icebox", icon: IceboxMap },
+  { value: 6, label: "Split", icon: SplitMap },
+  { value: 7, label: "Fracture", icon: FractureMap },
+];
 
 export const TAG_LIST = [
-    { value: 1, label: "Attacking" },
-    { value: 2, label: "Defending" },
-    { value: 3, label: "Post Plant" },
-    { value: 4, label: "Retake" },
-    { value: 16, label: "Fake" },
-    { value: 5, label: "A Site" },
-    { value: 6, label: "B Site" },
-    { value: 7, label: "C Site" },
-    { value: 8, label: "Mid" },
-    { value: 9, label: "Easy" },
-    { value: 10, label: "Medium" },
-    { value: 11, label: "Hard" },
-    { value: 12, label: "Crosshair Lineup" },
-    { value: 13, label: "UI Lineup" },
-    { value: 14, label: "Double Shock Dart" },
-    { value: 15, label: "Single Shock Dart" },
-]
+  { value: 1, label: "Attacking" },
+  { value: 2, label: "Defending" },
+  { value: 3, label: "Post Plant" },
+  { value: 4, label: "Retake" },
+  { value: 16, label: "Fake" },
+  { value: 5, label: "A Site" },
+  { value: 6, label: "B Site" },
+  { value: 7, label: "C Site" },
+  { value: 8, label: "Mid" },
+  { value: 9, label: "Easy" },
+  { value: 10, label: "Medium" },
+  { value: 11, label: "Hard" },
+  { value: 12, label: "Crosshair Lineup" },
+  { value: 13, label: "UI Lineup" },
+  { value: 14, label: "Double Shock Dart" },
+  { value: 15, label: "Single Shock Dart" },
+];
--- src/pages/LineupSite.js
+++ src/pages/LineupSite.js
@@ -1,27 +1,26 @@
 // @ts-nocheck
 import React, { Component } from "react";
 import ContentFrame from "../component-utils/lineup-site-utils/ContentFrame";
-import MapInteractionCSS from '../component-utils/map-utils/MapInteractionCSS'
+import MapInteractionCSS from "../component-utils/map-utils/MapInteractionCSS";
 import Map from "../component-utils/map-utils/Map";
 import Marker from "../component-utils/map-utils/Marker";
 import MultiSelect from "react-multi-select-component";
 import * as CONSTANTS from "../component-utils/constants";
-import Select from 'react-select'
+import Select from "react-select";
 
 export class LineupSite extends Component {
-
   customStyles = {
     container: (styles) => ({
       ...styles,
-      width: '25%',
-      height: '45px',
-      paddingRight: '5px'
+      width: "25%",
+      height: "45px",
+      paddingRight: "5px",
     }),
     control: (styles) => ({
       ...styles,
-      height: '100%',
-    })
-  }
+      height: "100%",
+    }),
+  };
 
   state = {
     savedLineups: {},
@@ -36,10 +35,10 @@ export class LineupSite extends Component {
     activeMarkerId: null, // used in content frame
     tags: [],
     images: [],
-    video: '',
-    description: '',
-    name: '',
-    credits: '',
+    video: "",
+    description: "",
+    name: "",
+    credits: "",
     mapArrowVisible: false, // red svg line from lineup position to start position
     selectedMarkerId: null, // used to keep arrow visible when leaving marker after clicking
     mapArrowPos: {
@@ -49,68 +48,80 @@ export class LineupSite extends Component {
       startY: -1,
     },
     markerScale: 1,
-  }
+  };
 
   lineupRetrievalRetries = 0;
 
   componentDidMount() {
-
-    document.title = "View Lineups"
+    document.title = "View Lineups";
 
     // attempt to read lineup info from localstorage and check expiration date
-    const localStorageLineups = localStorage.getItem('savedLineups');
-    const localStorageLineupsExpirationDate = localStorage.getItem('savedLineupsExpiration');
-
+    const localStorageLineups = localStorage.getItem("savedLineups");
+    const localStorageLineupsExpirationDate = localStorage.getItem(
+      "savedLineupsExpiration"
+    );
 
     // check if it is time to refresh lineups
-    if (localStorageLineups === null || localStorageLineupsExpirationDate === null || localStorageLineupsExpirationDate.toString().toLowerCase() === 'never' || Date.now() >= localStorageLineupsExpirationDate) {
-      console.log(`refreshing lineups, expiration in ${CONSTANTS.localStorageExpirationTime / 60 / 1000} minutes`);
+    if (
+      localStorageLineups === null ||
+      localStorageLineupsExpirationDate === null ||
+      localStorageLineupsExpirationDate.toString().toLowerCase() === "never" ||
+      Date.now() >= localStorageLineupsExpirationDate
+    ) {
+      console.log(
+        `refreshing lineups, expiration in ${
+          CONSTANTS.localStorageExpirationTime / 60 / 1000
+        } minutes`
+      );
       localStorage.clear();
 
       // store lineups by map in this object, and write it to the state
       let categorizedLineups = {};
 
       this.getLineupsFromDatabase((lineups) => {
-
-        lineups.forEach(lineup => {
-          let mapId = lineup['mapId'];
+        lineups.forEach((lineup) => {
+          let mapId = lineup["mapId"];
           if (mapId in categorizedLineups) {
-            categorizedLineups[mapId].push(lineup)
+            categorizedLineups[mapId].push(lineup);
           } else {
-            categorizedLineups[mapId] = [lineup]
+            categorizedLineups[mapId] = [lineup];
           }
         });
 
         this.setState({
-          savedLineups: categorizedLineups
-        })
+          savedLineups: categorizedLineups,
+        });
 
-        localStorage.setItem('savedLineups', JSON.stringify(categorizedLineups))
-        localStorage.setItem('savedLineupsExpiration', Date.now() + CONSTANTS.localStorageExpirationTime)
-
-      })
+        localStorage.setItem(
+          "savedLineups",
+          JSON.stringify(categorizedLineups)
+        );
+        localStorage.setItem(
+          "savedLineupsExpiration",
+          Date.now() + CONSTANTS.localStorageExpirationTime
+        );
+      });
     }
     // otherwise just retrieve lineups from local storage
     else {
       this.setState({
-        savedLineups: JSON.parse(localStorageLineups)
-      })
+        savedLineups: JSON.parse(localStorageLineups),
+      });
     }
-
   }
 
   getLineupsFromDatabase(callback) {
     let requestOptions = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    }
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
 
     fetch(`${CONSTANTS.API_URL}`, requestOptions)
-      .then(response => response.json())
-      .then(data => callback(data))
-      .catch(err => {
-        console.log('error retrieving lineups:', err)
-      })
+      .then((response) => response.json())
+      .then((data) => callback(data))
+      .catch((err) => {
+        console.log("error retrieving lineups:", err);
+      });
   }
 
   onMapClick = (e) => {
@@ -120,27 +131,28 @@ export class LineupSite extends Component {
 
     console.log(x, y)
     */
-  }
+  };
 
   onMarkerClick = (marker) => {
-
-    this.setState({
-      // clear images so they will be blank until loaded
-      images: []
-    }, () => {
-      this.setState({
-        activeMarkerId: marker.id,
-        selectedMarkerId: marker.id,
-        images: marker.images,
-        video: marker.video,
-        name: marker.name,
-        description: marker.description,
-        credits: marker.credits,
-        mapArrowVisible: true
-      })
-    })
-
-  }
+    this.setState(
+      {
+        // clear images so they will be blank until loaded
+        images: [],
+      },
+      () => {
+        this.setState({
+          activeMarkerId: marker.id,
+          selectedMarkerId: marker.id,
+          images: marker.images,
+          video: marker.video,
+          name: marker.name,
+          description: marker.description,
+          credits: marker.credits,
+          mapArrowVisible: true,
+        });
+      }
+    );
+  };
 
   onMarkerHover = (marker) => {
     // if start position not given
@@ -154,12 +166,11 @@ export class LineupSite extends Component {
         x: marker.x + adjustment,
         y: marker.y + adjustment,
         startX: marker.startX + adjustment,
-        startY: marker.startY + adjustment
+        startY: marker.startY + adjustment,
       },
-      mapArrowVisible: true
-    })
-
-  }
+      mapArrowVisible: true,
+    });
+  };
 
   onMarkerOut = (marker) => {
     // hide arrow when leaving non-selected marker
@@ -168,15 +179,14 @@ export class LineupSite extends Component {
     if (this.state.selectedMarkerId !== marker.id) {
       this.setState({
         selectedMarkerId: null,
-        mapArrowVisible: false
-      })
+        mapArrowVisible: false,
+      });
     }
-  }
+  };
 
   onMapSwitch = (e) => {
+    let selectedMap = e.value;
 
-    let selectedMap = e.value
-    
     if (selectedMap === this.state.mapId) {
       return;
     }
@@ -187,62 +197,64 @@ export class LineupSite extends Component {
       mapArrowVisible: false,
       visibleMarkers: [],
       tags: [],
-      name: '',
-      description: '',
-      credits: '',
+      name: "",
+      description: "",
+      credits: "",
       activeMarkerId: null,
       selectedMarkerId: null,
       images: [],
-      video: '',
-    })
+      video: "",
+    });
 
     // update map image, Map will automatically call updateMap after image has been loaded
     this.setState({
-      mapId: selectedMap
-    })
-
-
-  }
+      mapId: selectedMap,
+    });
+  };
 
   onAgentChange = (agent) => {
     // agent = { value: number, label: string }
 
     // clear map and update abilities
-    this.setState({
-      visibleMarkers: [],
-      agentId: agent.value,
-      abilityId: 0,
-      selectedAbility: null,
-      abilityList: CONSTANTS.ABILITY_LIST[agent.value]
-    }, this.updateMap)
-
-  }
+    this.setState(
+      {
+        visibleMarkers: [],
+        agentId: agent.value,
+        abilityId: 0,
+        selectedAbility: null,
+        abilityList: CONSTANTS.ABILITY_LIST[agent.value],
+      },
+      this.updateMap
+    );
+  };
 
   onAbilityChange = (ability) => {
-
     // ability = { value: number, label: string }
 
-    this.setState({
-      abilityId: ability.value,
-      selectedAbility: ability
-    }, this.updateMap);
-
-  }
+    this.setState(
+      {
+        abilityId: ability.value,
+        selectedAbility: ability,
+      },
+      this.updateMap
+    );
+  };
 
   onTagChange = (newTags) => {
-    this.setState({
-      tags: newTags
-    }, this.updateMap)
-
-  }
+    this.setState(
+      {
+        tags: newTags,
+      },
+      this.updateMap
+    );
+  };
 
   updateMap = () => {
-
     // make sure map and agent is selected
     if (this.state.agentId === 0 || this.state.mapId === 0) {
       this.setState({
-        visibleMarkers: []
-      })
+        visibleMarkers: [],
+      });
       return;
     }
 
@@ -257,144 +269,207 @@ export class LineupSite extends Component {
         setTimeout(this.updateMap, 1000); // retry updating map after 1 seconds
         return;
       }
-    } else if (!(this.state.mapId in this.state.savedLineups) || this.state.savedLineups[this.state.mapId].length === 0) {
+    } else if (
+      !(this.state.mapId in this.state.savedLineups) ||
+      this.state.savedLineups[this.state.mapId].length === 0
+    ) {
       return;
     }
 
     // get marker list and filter based on agent/ability
-    let filteredList = this.state.savedLineups[this.state.mapId].filter(marker => {
-      // Check agent and ability matches
-      if (parseInt(marker.agent) !== this.state.agentId) return false;
-      if (this.state.abilityId !== 0 && parseInt(marker.ability) !== this.state.abilityId) return false;
-      if (this.state.hiddenMarkers.includes(marker.id)) return false;
-      return true;
-    })
+    let filteredList = this.state.savedLineups[this.state.mapId].filter(
+      (marker) => {
+        // Check agent and ability matches
+        if (parseInt(marker.agent) !== this.state.agentId) return false;
+        if (
+          this.state.abilityId !== 0 &&
+          parseInt(marker.ability) !== this.state.abilityId
+        )
+          return false;
+        if (this.state.hiddenMarkers.includes(marker.id)) return false;
+        return true;
+      }
+    );
 
     // filter based on tags
     let selectedTagsList = this.state.tags.map((pair) => pair.value);
     if (selectedTagsList.length !== 0) {
       filteredList = filteredList.filter((marker) => {
-        return selectedTagsList.every((selectedTag) => marker.tags.includes(selectedTag));
-      })
+        return selectedTagsList.every((selectedTag) =>
+          marker.tags.includes(selectedTag)
+        );
+      });
     }
-
 
     this.setState({
       mapArrowVisible: false,
-      visibleMarkers: filteredList
-    })
-
-  }
+      visibleMarkers: filteredList,
+    });
+  };
 
   onFilterClick = (e) => {
     e.preventDefault();
-  }
+  };
 
   getMarkers = () => {
-    return (
-      this.state.visibleMarkers.map((marker) =>
-        <Marker
-          key={marker.id}
-          lineup={marker}
-          rotation={this.state.mapRotation}
-          onClick={() => this.onMarkerClick(marker)}
-          onHover={() => this.onMarkerHover(marker)}
-          onLeave={() => this.onMarkerOut(marker)}
-          scale={this.state.markerScale} />)
-    )
-  }
+    return this.state.visibleMarkers.map((marker) => (
+      <Marker
+        key={marker.id}
+        lineup={marker}
+        rotation={this.state.mapRotation}
+        onClick={() => this.onMarkerClick(marker)}
+        onHover={() => this.onMarkerHover(marker)}
+        onLeave={() => this.onMarkerOut(marker)}
+        scale={this.state.markerScale}
+      />
+    ));
+  };
 
   updateScale = (scale) => {
     if (scale !== this.state.markerScale) {
       this.setState({
-        markerScale: scale
-      })
+        markerScale: scale,
+      });
     }
-  }
+  };
 
   updateLineupState = (newState) => {
-    this.setState({
-      newState
-    }, this.updateMap)
-  }
+    this.setState(
+      {
+        newState,
+      },
+      this.updateMap
+    );
+  };
 
   clearHiddenMarkers = () => {
-    this.setState({
-      hiddenMarkers: []
-    }, this.updateMap)
-  }
+    this.setState(
+      {
+        hiddenMarkers: [],
+      },
+      this.updateMap
+    );
+  };
 
   rotateMapLeft = () => {
-    let newRotation = this.state.mapRotation - 90
-    if (newRotation < 0) newRotation += 360
+    let newRotation = this.state.mapRotation - 90;
+    if (newRotation < 0) newRotation += 360;
     this.setState({
-      mapRotation: newRotation
-    })
-  }
+      mapRotation: newRotation,
+    });
+  };
 
   rotateMapRight = () => {
-    let newRotation = this.state.mapRotation + 90
-    if (newRotation >= 360) newRotation -= 360
+    let newRotation = this.state.mapRotation + 90;
+    if (newRotation >= 360) newRotation -= 360;
     this.setState({
-      mapRotation: newRotation
-    })
-  }
+      mapRotation: newRotation,
+    });
+  };
 
   render() {
-
     return (
-      <div className='outer-frame'>
-        <div className='map-frame'>
-          <div className='filters-frame' >
-            <Select label="Map select" defaultValue={CONSTANTS.MAP_LIST[0]} options={CONSTANTS.MAP_LIST} styles={this.customStyles} onChange={this.onMapSwitch} />
-            <Select label="Agent select" placeholder='Agent...' options={CONSTANTS.AGENT_LIST} styles={this.customStyles} onChange={this.onAgentChange} />
-            <Select value={this.state.selectedAbility} label="Ability select" placeholder='Ability...' options={this.state.abilityList} styles={this.customStyles} onChange={this.onAbilityChange} />
+      <div className="outer-frame">
+        <div className="map-frame">
+          <div className="filters-frame">
+            <Select
+              label="Map select"
+              defaultValue={CONSTANTS.MAP_LIST[0]}
+              options={CONSTANTS.MAP_LIST}
+              styles={this.customStyles}
+              onChange={this.onMapSwitch}
+            />
+            <Select
+              label="Agent select"
+              placeholder="Agent..."
+              options={CONSTANTS.AGENT_LIST}
+              styles={this.customStyles}
+              onChange={this.onAgentChange}
+            />
+            <Select
+              value={this.state.selectedAbility}
+              label="Ability select"
+              placeholder="Ability..."
+              options={this.state.abilityList}
+              styles={this.customStyles}
+              onChange={this.onAbilityChange}
+            />
             <MultiSelect
-              className='lineup-filter-multi-select'
+              className="lineup-filter-multi-select"
               options={CONSTANTS.TAG_LIST}
               value={this.state.tags}
               labelledBy="Select"
               hasSelectAll={false}
               disableSearch={false}
-              overrideStrings={{ 'selectSomeItems': 'Filters...' }}
-              onChange={this.onTagChange} />
+              overrideStrings={{ selectSomeItems: "Filters..." }}
+              onChange={this.onTagChange}
+            />
           </div>
-          <button id='clear-hidden-markers-button' onClick={this.clearHiddenMarkers}>Clear hidden markers</button>
-          <div className='rotate-button-container'>
-            <button className='rotate-map-button' onClick={this.rotateMapLeft}>Rotate left</button>
-            <button className='rotate-map-button' onClick={this.rotateMapRight}>Rotate right</button>
+          <button
+            id="clear-hidden-markers-button"
+            onClick={this.clearHiddenMarkers}
+          >
+            Clear hidden markers
+          </button>
+          <div className="rotate-button-container">
+            <button className="rotate-map-button" onClick={this.rotateMapLeft}>
+              Rotate left
+            </button>
+            <button className="rotate-map-button" onClick={this.rotateMapRight}>
+              Rotate right
+            </button>
           </div>
-          <MapInteractionCSS
-            updateScale={this.updateScale}
-            maxScale={15}>
-            <div id='lineup-site-map'>
-              <Map rotation={this.state.mapRotation} mapId={this.state.mapId} onMapClick={this.onMapClick} updateMap={this.updateMap} />
+          <MapInteractionCSS updateScale={this.updateScale} maxScale={15}>
+            <div id="lineup-site-map">
+              <Map
+                rotation={this.state.mapRotation}
+                mapId={this.state.mapId}
+                onMapClick={this.onMapClick}
+                updateMap={this.updateMap}
+              />
 
-              {this.state.mapArrowVisible &&
-                <svg width="1000" height="1000" style={{ transform: `rotate(${this.state.mapRotation}deg`, position: 'fixed' }}>
-                  <line x1={this.state.mapArrowPos.x} y1={this.state.mapArrowPos.y} x2={this.state.mapArrowPos.startX} y2={this.state.mapArrowPos.startY} stroke="red" />
+              {this.state.mapArrowVisible && (
+                <svg
+                  width="1000"
+                  height="1000"
+                  style={{
+                    transform: `rotate(${this.state.mapRotation}deg`,
+                    position: "fixed",
+                  }}
+                >
+                  <line
+                    x1={this.state.mapArrowPos.x}
+                    y1={this.state.mapArrowPos.y}
+                    x2={this.state.mapArrowPos.startX}
+                    y2={this.state.mapArrowPos.startY}
+                    stroke="red"
+                  />
                 </svg>
-              }
+              )}
 
-              <div className='fixed-marker-frame' style={{ transform: `rotate(${this.state.mapRotation}deg)` }} >
+              <div
+                className="fixed-marker-frame"
+                style={{ transform: `rotate(${this.state.mapRotation}deg)` }}
+              >
                 {this.getMarkers()}
               </div>
             </div>
           </MapInteractionCSS>
         </div>
 
-        <ContentFrame updateParentState={this.updateLineupState}
+        <ContentFrame
+          updateParentState={this.updateLineupState}
           activeMarkerId={this.state.activeMarkerId}
           hiddenMarkers={this.state.hiddenMarkers}
           name={this.state.name}
           description={this.state.description}
           credits={this.state.credits}
           video={this.state.video}
-          images={this.state.images} />
-      </div >
+          images={this.state.images}
+        />
+      </div>
     );
   }
-
 }
 
 export default LineupSite;
