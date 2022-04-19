a362f15d68b73e7228ce98b72c2561a322ffaf42
matthew-paul <matthew.paul.mail@gmail.com>
2022-04-19 16:25:47 -0500

    update scale

--- src/component-utils/lineup-site-utils/ImageFrame.js
+++ src/component-utils/lineup-site-utils/ImageFrame.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import PinchZoomPan from '../responsive-pinch-zoom-pan/PinchZoomPan'
-import disableScroll from 'disable-scroll'
-
+import React from "react";
+import PinchZoomPan from "../responsive-pinch-zoom-pan/PinchZoomPan";
+import disableScroll from "disable-scroll";
 
 export const ImageFrame = (props) => {
+  const handleEnter = () =>
+    disableScroll["on"](document.getElementById("content-frame"));
+  const handleLeave = () => disableScroll["off"]();
 
-    const handleEnter = () => disableScroll['on'](document.getElementById('content-frame'))
-    const handleLeave = () => disableScroll['off']()
+  return (
+    <div className="image-frame">
+      <PinchZoomPan
+        doubleTapBehavior="zoom"
+        position="center"
+        initialScale={1}
+        minScale={1}
+        maxScale={15}
+        zoomButtons={false}
+      >
+        <img
+          className="lineup-image"
+          src={props.image}
+          alt="lineup info"
+          onMouseEnter={handleEnter}
+          onMouseLeave={handleLeave}
+        />
+      </PinchZoomPan>
+    </div>
+  );
+};
 
-    return (
-        <div className='image-frame' >
-            <PinchZoomPan doubleTapBehavior='zoom' position='center' initialScale={1} minScale={1} maxScale={10} zoomButtons={false}>
-                <img
-                    className='lineup-image'
-                    src={props.image} alt='lineup info'
-                    onMouseEnter={handleEnter}
-                    onMouseLeave={handleLeave} />
-            </PinchZoomPan>
-        </div>
-    )
-}
-
-export default ImageFrame
+export default ImageFrame;
